a0f026ccb3459c81d29712f02477b88aec779b5e
Raj Kishore Patra <rajkishorepatra@C02Y715EJGH8.local>
2024-05-25 18:14:51 +0530

    hero section

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Hero from "@/components/Hero";
+import NavBar from "@/components/NavBar";
+import './globals.css'
+
+export default function Home() {
+  return (
+    <>
+    <NavBar/>
+      <Hero/>
+    </>
+  );
+}
