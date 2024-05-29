1254e6f82377ac99a928f52f1a2e021d00c5ae02
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2024-05-29 21:11:32 +0530

    offer page on going

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,14 +1,126 @@
-import React from 'react'
-import Heading from './Heading'
+import EastIcon from "@mui/icons-material/East";
+import { motion } from "framer-motion";
+import {
+	bills,
+	pharmacy,
+	record,
+	reminder,
+	schedule,
+	software,
+	warehouse,
+} from "../assets";
 
 const Offer = () => {
-  return (
-    <>
-    <div className='py-10 my-5'>
-    <Heading start={"What We"} end={"Offer?"} />
-    </div>
-    </>
-  )
-}
+	const saturationArr = [
+		{
+			id: 1,
+			title: "Web Development",
+			subtitle:
+				"Discover our custom mobile app development services. Speecializing in native iOS and Android, cross-platform, and hybrid apps, ",
+			img: schedule.src,
+		},
+		{
+			id: 1,
+			title: "Mobile Development",
+			subtitle:
+				"Discover our custom mobile app development services. Speecializing in native iOS and Android, cross-platform, and hybrid apps, ",
+			img: reminder.src,
+		},
+		{
+			id: 1,
+			title: "UI/UX Design",
+			subtitle:
+				"Discover our custom mobile app development services. Speecializing in native iOS and Android, cross-platform, and hybrid apps",
+			img: schedule.src,
+		},
+		{
+			id: 1,
+			title: "UI/UX Design",
+			subtitle:
+				"Our highly efficient UI/UX designers craft beautiful applications that promise astounding user experience. We consistently brain-storm on building elite designs that are functionally simple to use.",
+			img: bills.src,
+		},
+		{
+			id: 2,
+			title: "UI/UX Design",
+			subtitle:
+				"Our highly efficient UI/UX designers craft beautiful applications that promise astounding user experience. We consistently brain-storm on building elite designs that are functionally simple to use.",
+			img: record.src,
+		},
+		{
+			id: 2,
+			title: "UI/UX Design",
+			subtitle:
+				"Our highly efficient UI/UX designers craft beautiful applications that promise astounding user experience. We consistently brain-storm on building elite designs that are functionally simple to use.",
+			img: software.src,
+		},
+		{
+			id: 2,
+			title: "UI/UX Design",
+			subtitle:
+				"Our highly efficient UI/UX designers craft beautiful applications that promise astounding user experience. We consistently brain-storm on building elite designs that are functionally simple to use.",
+			img: warehouse.src,
+		},
+		{
+			id: 2,
+			title: "UI/UX Design",
+			subtitle:
+				"Our highly efficient UI/UX designers craft beautiful applications that promise astounding user experience. We consistently brain-storm on building elite designs that are functionally simple to use.",
+			img: pharmacy.src,
+		},
+	];
 
-export default Offer
+	return (
+		<div
+			className="bg-blend-overlay bg-white/80 w-full py-5 md:pb-20 lg:h-[50vw] flex flex-col justify-evenly items-center bg-cover bg-center bg-no-repeat"
+			id="product"
+		>
+			<div className="flex flex-col justify-center items-center">
+				<div className="flex items-center gap-2 mt-10 text-sm text-[#0E89E2] font-bold">
+					<div className="w-8 h-[2px] bg-[#0E89E2] rounded-lg"></div>
+					<div>Developing Solutions For The Future</div>
+					<div className="w-8 h-[2px] bg-[#0E89E2] rounded-lg"></div>
+				</div>
+				<h2 className="lg:text-4xl text-xl font-bold text-[#0E89E2] text-center mb-8">
+					<span className="lg:text-4xl text-xl font-thin text-black text-center">
+						What
+					</span>{" "}
+					We Offer
+				</h2>
+			</div>
+			<div className="grid md:grid-cols-2 lg:grid-cols-4 grid-cols-1 md:gap-7 lg:gap-10 justify-items-center">
+				{saturationArr.map((item, index) => {
+					return (
+						<motion.div
+							// data-aos="flip-right"
+							key={index}
+							initial={{ scale: 0, opacity: 0, rotateY: 0 }}
+							animate={{ scale: 1, opacity: 1, rotateY: 360 }}
+							// whileHover={{ scale: 1.1 }}
+							transition={{ delay: index * 0.1 }}
+							exit={{ scale: 0, opacity: 0, rotateY: 0 }}
+							className="shadow-xl group px-2 rounded-xl flex flex-col gap-3 justify-center items-start w-72 md:w-56 h-64 hover:shadow-2xl border-2 transition duration-500 ease-out"
+						>
+							<div className="bg-[#0E89E2] flex justify-center items-center p-2 w-10 h-10 rounded-xl">
+								<img src={item.img} alt="product-img" />
+							</div>
+
+							<p className="text-md font-bold text-gray-700 hover:text-transparent hover:bg-clip-text hover:bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-indigo-700 via-pink-600 to-zinc-300 transition duration-500 ease-out">
+								{item.title}
+							</p>
+							<p className="text-xs text-gray-500 transition duration-500 ease-out">
+								{item.subtitle}
+							</p>
+							<div className="px-3 py-2 transition duration-500 ease-out text-sm font-medium hover:text-[#2d64a6]">
+								<button className="px-2">View Details</button>
+								<EastIcon />
+							</div>
+						</motion.div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
+
+export default Offer;
